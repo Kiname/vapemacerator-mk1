e365151aca3e69384bb3b4eb4c9e446f761e5f27
oscargonzalez <oscar.gonzalez.varela@gmail.com>
2015-08-26 23:54:04 +0200

    Removed rotation direction. Added Cycle and Duration Added hours/minutes visualisation Fixed read/write settings from EEPROM

--- src/controller/controller.ino
+++ src/controller/controller.ino
@@ -28,12 +28,26 @@
 
 #define LCD_BAUDRATE      9600
 
+// Thanks to Mem for this! (http://forum.arduino.cc/index.php?topic=42211.0)
+#define SECS_PER_MIN  (60UL)
+#define SECS_PER_HOUR (3600UL)
+#define SECS_PER_DAY  (SECS_PER_HOUR * 24L)
+
+/* Useful Macros for getting elapsed time */
+#define numberOfSeconds(_time_) (_time_ % SECS_PER_MIN)  
+#define numberOfMinutes(_time_) ((_time_ / SECS_PER_MIN) % SECS_PER_MIN) 
+#define numberOfHours(_time_) (( _time_% SECS_PER_DAY) / SECS_PER_HOUR)
+#define elapsedDays(_time_) ( _time_ / SECS_PER_DAY)  
+
+#define CYCLE_MAX               54  // In steps of 10 mins. 54*10 = 540 minutes = 9 hours
+#define DURATION_MAX            54  // In steps of 10 mins. 54*10 = 540 minutes = 9 hours
+
 // All seting vslues.
 int   setting_microstep         = 0;
 char *setting_microstep_str[]   = {"1/1", "1/2", "1/4", "1/8", "1/16"};
-int setting_rotatedir           = 0;
-char *setting_rotatedir_str[]   = {"Left", "Right"};
 int setting_speed               = 40;
+int setting_cycle               = 2;
+int setting_duration            = 2;
 int setting_mode                = 0;
 char *setting_mode_str[]        = {"Constant Speed", "Flip-Flop","Shake"};
 
@@ -71,14 +85,22 @@ long oldPosition  = -999;
  *          3     (1/8)
  *          4     (1/16)
  *          
- * Byte 3: Rotate dir        
- * Values:  0     (Left)
- *          1     (Right)
- *          
- * Byte 4: Speed
- * Values:  0 to 100
+ * Byte 3: Cycle
+ * Values:  1 to 50 
+ *          Multiply by 10 to get total cycle duration in minutes: Example: 5*10 = 50 minutes = 0.83 hours or 24*10 = 120 minutes = 4 hours
+ *          Display shows step of 10 minutes
  * 
- * Byte 5: Mode
+ * Byte 4: Duration
+ * Values:  1 to 100 
+ *          Multiply by 10 to get total duration in minutes: Same as Cycle
+ *          Display shows step of 10 minutes. Never less than Cycle duration!
+ * 
+ *  
+ * Byte 5: Speed
+ * Values:  0 to 100* 
+ *          
+ * 
+ * Byte 6: Mode
  * Values:  0     (Constant Speed)
  *          1     (Flip-Flop)
  *          2     (Shake)
@@ -91,9 +113,10 @@ void readParams()
   {
     Serial.println("Read values...");
     setting_microstep     = EEPROM.read(2);
-    setting_rotatedir     = EEPROM.read(3);
-    setting_speed         = EEPROM.read(4);
-    setting_mode          = EEPROM.read(5);  
+    setting_cycle         = EEPROM.read(3);
+    setting_duration      = EEPROM.read(4);
+    setting_speed         = EEPROM.read(5);
+    setting_mode          = EEPROM.read(6);  
   }
   else
   {
@@ -102,9 +125,10 @@ void readParams()
     EEPROM.write(0, 'M');
     EEPROM.write(1, 'K');
     EEPROM.write(2, setting_microstep);
-    EEPROM.write(3, setting_rotatedir);
-    EEPROM.write(4, setting_speed);
-    EEPROM.write(5, setting_mode);
+    EEPROM.write(3, setting_cycle);
+    EEPROM.write(4, setting_duration);
+    EEPROM.write(5, setting_speed);
+    EEPROM.write(6, setting_mode);
   }
   
 }
@@ -112,9 +136,10 @@ void readParams()
 void writeParams()
 {
     EEPROM.write(2, setting_microstep);
-    EEPROM.write(3, setting_rotatedir);
-    EEPROM.write(4, setting_speed);
-    EEPROM.write(5, setting_mode);  
+    EEPROM.write(3, setting_cycle);
+    EEPROM.write(4, setting_duration);
+    EEPROM.write(5, setting_speed);
+    EEPROM.write(6, setting_mode);  
 }
 
 // ************************************************************************************************************
@@ -178,12 +203,29 @@ void processMenu()
       case 2:
       {            
         LCDclear();  
-        LCDtext("Direction:",1);      
-        LCDtext(setting_rotatedir_str[setting_rotatedir], 2);        // Left [Right]
+        LCDtext("Cycle:",1);      
+        int hours = numberOfHours(setting_cycle*10*60);
+        int minutes = numberOfMinutes(setting_cycle*10*60);
+        sprintf(buf,"%02d Hours %02d Mins", hours, minutes);        
+        
+        LCDtext(buf, 2);        // Cycle in minutes
       }
       break;
-  
+
       case 3:
+      {            
+        LCDclear();  
+        LCDtext("Duration:",1);    
+
+        int hours = numberOfHours(setting_duration*10*60);
+        int minutes = numberOfMinutes(setting_duration*10*60);
+        sprintf(buf,"%02d Hours %02d Mins", hours, minutes);
+        
+        LCDtext(buf, 2);        // Duration in minutes
+      }
+      break;      
+  
+      case 4:
       {            
         LCDclear();  
         LCDtext("Speed:",1);
@@ -193,7 +235,7 @@ void processMenu()
       }
       break;
   
-      case 4:
+      case 5:
       {            
         LCDclear();  
         LCDtext("Mode:",1);
@@ -201,7 +243,7 @@ void processMenu()
       }
       break;                
   
-      case 5:
+      case 6:
       {            
         LCDclear();  
         LCDtext("Default values:",1);
@@ -217,7 +259,7 @@ void processMenu()
       menu_position = getEncoderValue();    
       
       if (menu_position < 0) { menu_position=0; encoder.write(0); }
-      if (menu_position > 5) { menu_position=5; encoder.write(5*4);}    
+      if (menu_position > 6) { menu_position=6; encoder.write(6*4);}    
     }
     else if (menu_mode == 1)
     {
@@ -233,20 +275,33 @@ void processMenu()
           break;
           
         case 2:          
-          setting_rotatedir = getEncoderValue();         
+          setting_cycle = getEncoderValue();         
           // Limit min/max values 
-          if (setting_rotatedir > 1) { setting_rotatedir=1; encoder.write(1*4); }
-          if (setting_rotatedir < 0) { setting_rotatedir=0; encoder.write(0); }        
+          if (setting_cycle > CYCLE_MAX) { setting_cycle=CYCLE_MAX; encoder.write(CYCLE_MAX*4); }
+          if (setting_cycle < 1) { setting_cycle=1; encoder.write(1*4); } 
+
+          // Cycle must be lower of equal to duration
+          if (setting_cycle > setting_duration) { setting_duration=setting_cycle; }
           break;        
+
+        case 3:          
+          setting_duration = getEncoderValue();         
+          // Limit min/max values 
+          if (setting_duration > DURATION_MAX) { setting_duration=DURATION_MAX; encoder.write(DURATION_MAX*4); }
+          if (setting_duration < 1) { setting_duration=1; encoder.write(1*4); }        
+
+          // Duration must be equal or greater than Cycle
+          if (setting_duration < setting_cycle) { setting_cycle=setting_duration; }
+          break;                  
           
-        case 3:
+        case 4:
           setting_speed = getEncoderValue();  
           // Limit min/max values 
           if (setting_speed > 250) { setting_speed=250; encoder.write(250*4); }
           if (setting_speed < 1)   { setting_speed=1; encoder.write(1*4); }
           break;
 
-        case 4:
+        case 5:
           setting_mode = getEncoderValue();  
           // Limit min/max values 
           if (setting_mode > 2) { setting_mode=2; encoder.write(2*4); }
@@ -261,8 +316,10 @@ void processMenu()
       { 
         menu_mode = 1; 
         if (menu_position == 1) { encoder.write(setting_microstep*4); }
-        if (menu_position == 2) {  }        
-        if (menu_position == 3) { encoder.write(setting_speed*4); }        
+        if (menu_position == 2) { encoder.write(setting_cycle*4); }        
+        if (menu_position == 3) { encoder.write(setting_duration*4); }        
+        if (menu_position == 4) { encoder.write(setting_speed*4); }        
+        if (menu_position == 5) { encoder.write(setting_mode*4); }                
       }
       else 
       { 
